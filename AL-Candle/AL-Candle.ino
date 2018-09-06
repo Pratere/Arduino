cdb17a94ad11e8edb579c8fb217297f88e5953f9
Eli Prater <pratere@berea.edu>
2018-09-05 23:03:43 -0400

    Make da code work

--- AL-Candle/AL-Candle.ino
+++ AL-Candle/AL-Candle.ino
@@ -1,5 +1,5 @@
-int total1;
-int total2;
+long int total1;
+long int total2;
 int state=1;
 void setup() {
   // put your setup code here, to run once:
@@ -13,19 +13,25 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (state=1){
     total1=0;
-    for (int i=0; i>=200; i++){
-      total1 += analogRead(A0); 
+    for (int i=0; i<=200; i++){
+      total1 += (analogRead(A0)/10); 
     }
+    Serial.print(total1);
     state = 2;
+    Serial.println();
   }
   if (state=2){
     total2=0;
-    for (int i=0; i>=200; i++){
-      total2 += analogRead(A0); 
+    for (int i=0; i<=200; i++){
+      total2 += (analogRead(A0)/10); 
     }
     state = 1;
+    Serial.print(total2);
+    Serial.println();
   }
-  if (abs(total1-total2) > 100){
+  if (abs(total1-total2) > 75){
     digitalWrite(3, LOW);
+    delay(2000);
+    digitalWrite(3, HIGH);
   }
 }
